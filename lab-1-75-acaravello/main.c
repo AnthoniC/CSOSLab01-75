8cc6f18c6837a0c7df6371b540565915d380c2f5
Anthoni <anthoni@anthonis-imac.local>
2019-03-20 06:16:56 -0500

    test

---
+++ lab-1-75-acaravello/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  lab-1-75-acaravello
+//
+//  Created by Anthoni on 3/17/19.
+//  Copyright © 2019 Anthoni. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    //edit
+    return 0;
+}
